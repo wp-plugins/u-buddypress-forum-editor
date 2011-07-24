56b463d8babab744411df25f05c2a1960bf0b05d
taehan <taehan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-24 03:11:22 +0000

    ubp-forum-editor 1.1
    git-svn-id: https://plugins.svn.wordpress.org/u-buddypress-forum-editor/tags/1.1@414396 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/allowed-tags.php
@@ -0,0 +1,482 @@
+<?php
+$default_allowedtags = array(
+'a',
+'abbr',
+'acronym',
+'b',
+'big',
+'blockquote',
+'br',
+'caption',
+'cite',
+'code',
+'del',
+'div',
+'em',
+'font',
+'hr',
+'i',
+'img',
+'li',
+'p',
+'pre',
+'span',
+'strike',
+'strong',
+'sub',
+'sup',
+'table',
+'tbody',
+'td',
+'th',
+'thead',
+'tr',
+'u',
+'ul',
+'ol',
+);
+
+
+
+$full_allowedtags = array(
+'address' => array(),
+'a' => array(
+	'class' => array (),
+	'href' => array (),
+	'id' => array (),
+	'title' => array (),
+	'rel' => array (),
+	'rev' => array (),
+	'name' => array (),
+	'target' => array()),
+'abbr' => array(
+	'class' => array (),
+	'title' => array ()),
+'acronym' => array(
+	'title' => array ()),
+'article' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'aside' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'b' => array(),
+'big' => array(),
+'blockquote' => array(
+	'id' => array (),
+	'cite' => array (),
+	'class' => array(),
+	'lang' => array(),
+	'xml:lang' => array()),
+'br' => array (
+	'class' => array ()),
+'button' => array(
+	'disabled' => array (),
+	'name' => array (),
+	'type' => array (),
+	'value' => array ()),
+'caption' => array(
+	'align' => array (),
+	'class' => array ()),
+'cite' => array (
+	'class' => array(),
+	'dir' => array(),
+	'lang' => array(),
+	'title' => array ()),
+'code' => array (
+	'style' => array()),
+'col' => array(
+	'align' => array (),
+	'char' => array (),
+	'charoff' => array (),
+	'span' => array (),
+	'dir' => array(),
+	'style' => array (),
+	'valign' => array (),
+	'width' => array ()),
+'del' => array(
+	'datetime' => array ()),
+'dd' => array(),
+'details' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'open' => array (),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'div' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array()),
+'dl' => array(),
+'dt' => array(),
+'em' => array(),
+'fieldset' => array(),
+'figure' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'figcaption' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'font' => array(
+	'color' => array (),
+	'face' => array (),
+	'size' => array ()),
+'footer' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'form' => array(
+	'action' => array (),
+	'accept' => array (),
+	'accept-charset' => array (),
+	'enctype' => array (),
+	'method' => array (),
+	'name' => array (),
+	'target' => array ()),
+'h1' => array(
+	'align' => array (),
+	'class' => array (),
+	'id'    => array (),
+	'style' => array ()),
+'h2' => array (
+	'align' => array (),
+	'class' => array (),
+	'id'    => array (),
+	'style' => array ()),
+'h3' => array (
+	'align' => array (),
+	'class' => array (),
+	'id'    => array (),
+	'style' => array ()),
+'h4' => array (
+	'align' => array (),
+	'class' => array (),
+	'id'    => array (),
+	'style' => array ()),
+'h5' => array (
+	'align' => array (),
+	'class' => array (),
+	'id'    => array (),
+	'style' => array ()),
+'h6' => array (
+	'align' => array (),
+	'class' => array (),
+	'id'    => array (),
+	'style' => array ()),
+'header' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'hgroup' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'hr' => array (
+	'align' => array (),
+	'class' => array (),
+	'noshade' => array (),
+	'size' => array (),
+	'width' => array ()),
+'i' => array(),
+'img' => array(
+	'alt' => array (),
+	'align' => array (),
+	'border' => array (),
+	'class' => array (),
+	'height' => array (),
+	'hspace' => array (),
+	'longdesc' => array (),
+	'vspace' => array (),
+	'src' => array (),
+	'style' => array (),
+	'width' => array ()),
+'ins' => array(
+	'datetime' => array (),
+	'cite' => array ()),
+'kbd' => array(),
+'label' => array(
+	'for' => array ()),
+'legend' => array(
+	'align' => array ()),
+'li' => array (
+	'align' => array (),
+	'class' => array ()),
+'menu' => array (
+	'class' => array (),
+	'style' => array (),
+	'type' => array ()),
+'nav' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'p' => array(
+	'class' => array (),
+	'align' => array (),
+	'dir' => array(),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array()),
+'pre' => array(
+	'style' => array(),
+	'width' => array ()),
+'q' => array(
+	'cite' => array ()),
+'s' => array(),
+'span' => array (
+	'class' => array (),
+	'dir' => array (),
+	'align' => array (),
+	'lang' => array (),
+	'style' => array (),
+	'title' => array (),
+	'xml:lang' => array()),
+'section' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'strike' => array(),
+'strong' => array(),
+'sub' => array(),
+'summary' => array(
+	'align' => array (),
+	'class' => array (),
+	'dir' => array (),
+	'lang' => array(),
+	'style' => array (),
+	'xml:lang' => array(),
+),
+'sup' => array(),
+'table' => array(
+	'align' => array (),
+	'bgcolor' => array (),
+	'border' => array (),
+	'cellpadding' => array (),
+	'cellspacing' => array (),
+	'class' => array (),
+	'dir' => array(),
+	'id' => array(),
+	'rules' => array (),
+	'style' => array (),
+	'summary' => array (),
+	'width' => array ()),
+'tbody' => array(
+	'align' => array (),
+	'char' => array (),
+	'charoff' => array (),
+	'valign' => array ()),
+'td' => array(
+	'abbr' => array (),
+	'align' => array (),
+	'axis' => array (),
+	'bgcolor' => array (),
+	'char' => array (),
+	'charoff' => array (),
+	'class' => array (),
+	'colspan' => array (),
+	'dir' => array(),
+	'headers' => array (),
+	'height' => array (),
+	'nowrap' => array (),
+	'rowspan' => array (),
+	'scope' => array (),
+	'style' => array (),
+	'valign' => array (),
+	'width' => array ()),
+'textarea' => array(
+	'cols' => array (),
+	'rows' => array (),
+	'disabled' => array (),
+	'name' => array (),
+	'readonly' => array ()),
+'tfoot' => array(
+	'align' => array (),
+	'char' => array (),
+	'class' => array (),
+	'charoff' => array (),
+	'valign' => array ()),
+'th' => array(
+	'abbr' => array (),
+	'align' => array (),
+	'axis' => array (),
+	'bgcolor' => array (),
+	'char' => array (),
+	'charoff' => array (),
+	'class' => array (),
+	'colspan' => array (),
+	'headers' => array (),
+	'height' => array (),
+	'nowrap' => array (),
+	'rowspan' => array (),
+	'scope' => array (),
+	'valign' => array (),
+	'width' => array ()),
+'thead' => array(
+	'align' => array (),
+	'char' => array (),
+	'charoff' => array (),
+	'class' => array (),
+	'valign' => array ()),
+'title' => array(),
+'tr' => array(
+	'align' => array (),
+	'bgcolor' => array (),
+	'char' => array (),
+	'charoff' => array (),
+	'class' => array (),
+	'style' => array (),
+	'valign' => array ()),
+'tt' => array(),
+'u' => array(),
+'ul' => array (
+	'class' => array (),
+	'style' => array (),
+	'type' => array ()),
+'ol' => array (
+	'class' => array (),
+	'start' => array (),
+	'style' => array (),
+	'type' => array ()),
+'var' => array (),
+/*
+'script' => array(
+	'charset' => array (),
+	'defer' => array (),
+	'language' => array (),
+	'src' => array (),
+	'type' => array (),
+),
+'style' => array(
+	'lang' => array (),
+	'media' => array (),
+	'title' => array (),
+	'type' => array (),
+	'dir' => array (),
+),
+'link' => array(
+	'rel' => array (),
+	'href' => array (),
+	'type' => array (),
+	'media' => array (),
+	'dir' => array (),
+),
+*/
+'applet' => array(
+	'id' => array (),
+	'alt' => array (),
+	'archive' => array (),
+	'codebase' => array (),
+	'width' => array (),
+	'height' => array (),
+	'name' => array (),
+),
+'iframe' => array(
+	'class' => array (),
+	'frameborder' => array (),
+	'height' => array (),
+	'id' => array (),
+	'marginheight' => array (),
+	'marginwidth' => array (),
+	'name' => array (),
+	'scrolling' => array (),
+	'src' => array (),
+	'style' => array (),
+	'title' => array (),
+	'width' => array (),
+),
+'object' => array(
+	'classid' => array (),
+	'width' => array (),
+	'height' => array (),
+	'codebase' => array (),
+	'id' => array (),
+),
+'embed' => array(
+	'type' => array (),
+	'width' => array (),
+	'height' => array (),
+	'src' => array (),
+	'flashvar' => array (),
+	'allowscriptaccess' => array (),
+	'allowfullscreen' => array (),
+),
+'param' => array(
+	'name' => array (),
+	'value' => array (),
+),
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
